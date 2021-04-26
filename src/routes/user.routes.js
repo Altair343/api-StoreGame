bfe32875d8566238d60e6c3846704c9f94f3e5a3
Altair343 <and97_26@hotmail.com>
2021-04-25 23:19:09 -0500

    creando ruta para verificar token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,16 +2,28 @@ import { Router } from "express";
 const router = Router();
 
 import * as usersCtrl from "../controllers/userController";
-import { verifySignup } from "../middlewares";
+import { verifySignup, authJwt } from "../middlewares";
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Aquí es donde puedes registrar rutas API para la aplicación. 
-| 
+| Aquí es donde puedes registrar rutas API para la aplicación.
 |
 */
+
+/**
+ * Ruta para verificar el token
+ * 
+ */
+
+router.post('/verifyToken', authJwt.verifyToken , (req, res) => {
+    return res.status(200).json({
+        code: "token/verify",
+        response: true,
+        message: "token valid"
+    });
+});
 
 export default router;
